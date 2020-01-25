71f63b89f8e4617e09e42965c1c7ab1ef67c7cbf
SaharCarmel <sahar.carmel@levitection.com>
2020-01-25 17:55:01 +0200

    Added a feature to print the releveant code and plot figs

--- Utils/points_builder.py
+++ Utils/points_builder.py
@@ -8,31 +8,78 @@ def castYtofield(y):
 def castXtofield(x):
     return x/60
 
-fig = plt.figure()
-x = np.array([-1 , 1])
-y = np.array([-0.45, 0.45])
-# plt.scatter(x,y)
-plt.vlines(castXtofield(60-16.5), ymax=0.2, ymin=-0.2)
-plt.vlines(castXtofield(-60+16.5), ymax=0.2, ymin=-0.2)
-# 16 meter
-plt.hlines(0.2, castXtofield(-60), castXtofield(-60+16.5))
-plt.hlines(-0.2, castXtofield(-60), castXtofield(-60+16.5))
-plt.hlines(0.2, castXtofield(60), castXtofield(60-16.5))
-plt.hlines(-0.2, castXtofield(60), castXtofield(60-16.5))
-## inner
-plt.vlines(castXtofield(60-5.5), castYtofield(7.32/2+5.5), castYtofield(-7.32/2-5.5))
-plt.vlines(castXtofield(-60+5.5), castYtofield(7.32/2+5.5), castYtofield(-7.32/2-5.5))
-plt.hlines(castYtofield(7.32/2+5.5), castXtofield(-60), castXtofield(-60+5.5))
-plt.hlines(-castYtofield(7.32/2+5.5), castXtofield(-60), castXtofield(-60+5.5))
-plt.hlines(castYtofield(7.32/2+5.5), castXtofield(60), castXtofield(60-5.5))
-plt.hlines(-castYtofield(7.32/2+5.5), castXtofield(60), castXtofield(60-5.5))
-# Boundries
-plt.hlines(-0.45,1,-1)
-plt.hlines(0.45,1,-1)
-plt.vlines(1,0.45,-0.45)
-plt.vlines(0,0.45,-0.45)
-plt.vlines(-1,0.45,-0.45)
-# plt.grid()
-points = plt.ginput(3, show_clicks=True)
-print(points)
+def plotField():
+    fig = plt.figure()
+    x = np.array([-1 , 1])
+    y = np.array([-0.45, 0.45])
+    # plt.scatter(x,y)
+    plt.vlines(castXtofield(60-16.5), ymax=0.2, ymin=-0.2)
+    plt.vlines(castXtofield(-60+16.5), ymax=0.2, ymin=-0.2)
+    # 16 meter
+    plt.hlines(0.2, castXtofield(-60), castXtofield(-60+16.5))
+    plt.hlines(-0.2, castXtofield(-60), castXtofield(-60+16.5))
+    plt.hlines(0.2, castXtofield(60), castXtofield(60-16.5))
+    plt.hlines(-0.2, castXtofield(60), castXtofield(60-16.5))
+    ## inner
+    plt.vlines(castXtofield(60-5.5), castYtofield(7.32/2+5.5), castYtofield(-7.32/2-5.5))
+    plt.vlines(castXtofield(-60+5.5), castYtofield(7.32/2+5.5), castYtofield(-7.32/2-5.5))
+    plt.hlines(castYtofield(7.32/2+5.5), castXtofield(-60), castXtofield(-60+5.5))
+    plt.hlines(-castYtofield(7.32/2+5.5), castXtofield(-60), castXtofield(-60+5.5))
+    plt.hlines(castYtofield(7.32/2+5.5), castXtofield(60), castXtofield(60-5.5))
+    plt.hlines(-castYtofield(7.32/2+5.5), castXtofield(60), castXtofield(60-5.5))
+    # Boundries
+    plt.hlines(-0.45,1,-1)
+    plt.hlines(0.45,1,-1)
+    plt.vlines(1,0.45,-0.45)
+    plt.vlines(0,0.45,-0.45)
+    plt.vlines(-1,0.45,-0.45)
+    return fig
+
+
+## getting attackers
+attacking_players = int(input("Num of attacking players: "))
+defending_players = int(input("Num of defending players: "))
+fig = plotField()
+plt.title("Place attackers")
+attacking_points = plt.ginput(attacking_players, show_clicks=True)
+attacking_points = np.array(attacking_points).transpose()
+plt.close()
+
+## getting defenders
+fig = plotField()
+plt.title("Place defenders")
+plt.scatter(attacking_points[0], attacking_points[1], marker="+", c="blue")
+defending_points = plt.ginput(defending_players, show_clicks=True)
+defending_points = np.array(defending_points).transpose()
+plt.close()
+
+## getting ball
+fig = plotField()
+plt.title("Place ball")
+plt.scatter(attacking_points[0], attacking_points[1], marker="+", c="blue")
+plt.scatter(defending_points[0], defending_points[1], marker="_", c="red")
+ball_point = plt.ginput(1, show_clicks=True)
+
+plt.close()
+fig = plotField()
+plt.scatter(attacking_points[0], attacking_points[1], marker="+", c="blue")
+plt.scatter(defending_points[0], defending_points[1], marker="_", c="red")
+plt.scatter(ball_point[0][0], ball_point[0][1], marker="o", c="green")
 plt.show()
+fig.savefig("corner_2_vs_1_keeper.jpg")
+
+
+print("Ball")
+print("builder.SetBallPosition({:.4f}, {:.4f})".format(ball_point[0][0],ball_point[0][1]))
+
+print("Defending team")
+for item in defending_points.transpose():
+    print("builder.AddPlayer({:.4f}, {:.4f}, e_PlayerRole_GK)".format(item[0], item[1]))
+
+print("Attacking team")
+for item in attacking_points.transpose():
+    print("builder.AddPlayer({:.4f}, {:.4f}, e_PlayerRole_GK)".format(item[0], item[1]))
+
+
+x = 2
+
